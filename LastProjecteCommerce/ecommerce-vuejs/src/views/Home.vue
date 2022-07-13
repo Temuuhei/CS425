04b88cd509b10fde123c287dfe7b879198ac6469
Khosbayar <xocoooo@gmail.com>
2022-07-13 18:01:48 -0500

    changa some UI

--- LastProjecteCommerce/ecommerce-vuejs/src/views/Home.vue
+++ LastProjecteCommerce/ecommerce-vuejs/src/views/Home.vue
@@ -5,9 +5,10 @@
 
       <div class="container py-5">
         <header class="text-left text-white py-5">
-          <h3 class="mb-4 rounded"><a href="#start-shopping" class="bg-white px-2 py-2 rounded" id="heading">Start Shopping</a></h3>
+          <h3 class="mb-4 rounded"><a href="#start-shopping" class="bg-white px-2 py-2 rounded" id="heading">CS425-SWE
+              Project. Online Shopping</a></h3>
           <div id="content" class="lead mb-0 bg-dark p-1 rounded">
-            <p>CS425-SWE Project. Online Shopping</p>
+            <h4>Team members:</h4>
             <ul>
               <li>Khosbayar Sandag</li>
               <li>Tamir Bold</li>
@@ -25,8 +26,9 @@
         </div>
       </div>
       <div class="row">
-        <div v-for="index in this.category_size" :key="index" class="col-md-6 col-xl-4 col-12 pt-3  justify-content-around d-flex">
-          <CategoryBox :category="categories[index-1]">
+        <div v-for="index in this.category_size" :key="index"
+          class="col-md-6 col-xl-4 col-12 pt-3  justify-content-around d-flex">
+          <CategoryBox :category="categories[index - 1]">
           </CategoryBox>
         </div>
       </div>
@@ -40,8 +42,9 @@
         </div>
       </div>
       <div class="row">
-        <div v-for="index in this.product_size" :key="index" class="col-md-6 col-xl-4 col-12 pt-3  justify-content-around d-flex">
-          <ProductBox :product="products[index-1]">
+        <div v-for="index in this.product_size" :key="index"
+          class="col-md-6 col-xl-4 col-12 pt-3  justify-content-around d-flex">
+          <ProductBox :product="products[index - 1]">
           </ProductBox>
         </div>
       </div>
@@ -50,54 +53,54 @@
 </template>
 
 <script>
-  import ProductBox from "../components/Product/ProductBox";
-  import CategoryBox from "../components/Category/CategoryBox";
-  export default {
-    name: 'Home',
-    components : { ProductBox, CategoryBox},
-    props : ["baseURL", "products", "categories"],
-    data(){
-      return{
-        category_size:0,
-        product_size:0
-      }
-    },
-    mounted(){
-      this.category_size = this.categories.length;
-      this.category_size = Math.min(6, this.category_size);
-
-      this.product_size = this.products.length;
-      this.product_size = Math.min(8, this.product_size);
+import ProductBox from "../components/Product/ProductBox";
+import CategoryBox from "../components/Category/CategoryBox";
+export default {
+  name: 'Home',
+  components: { ProductBox, CategoryBox },
+  props: ["baseURL", "products", "categories"],
+  data() {
+    return {
+      category_size: 0,
+      product_size: 0
     }
+  },
+  mounted() {
+    this.category_size = this.categories.length;
+    this.category_size = Math.min(6, this.category_size);
+
+    this.product_size = this.products.length;
+    this.product_size = Math.min(8, this.product_size);
   }
+}
 </script>
 
 <style>
-  .page-holder {
-    min-height: 100vh;
-  }
+.page-holder {
+  min-height: 100vh;
+}
 
-  .bg-cover {
-    background-size: cover !important;
-  }
+.bg-cover {
+  background-size: cover !important;
+}
 
-  #background-div {
-    background: url(../assets/homebg.jpg)
-  }
+#background-div {
+  background: url(../assets/homecover.jpeg)
+}
 
-  #heading {
-    text-decoration: none;
-    font-family: 'Roboto', sans-serif;
-    font-weight: 400;
-    opacity: 0.8;
-    font-family: 'Josefin Sans', sans-serif;
-  }
+#heading {
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  opacity: 0.8;
+  font-family: 'Josefin Sans', sans-serif;
+}
 
-  #content {
-    opacity: 0.8;
-  }
+#content {
+  opacity: 0.8;
+}
 
-  h2 {
-    font-family: 'Josefin Sans', sans-serif;
-  }
+h2 {
+  font-family: 'Josefin Sans', sans-serif;
+}
 </style>
--- LastProjecteCommerce/ecommerce-vuejs/src/views/Signin.vue
+++ LastProjecteCommerce/ecommerce-vuejs/src/views/Signin.vue
@@ -16,47 +16,23 @@
           <form @submit="signin" class="pt-4 pl-4 pr-4">
             <div class="form-group">
               <label>Email</label>
-              <input
-                type="email"
-                class="form-control"
-                v-model="email"
-                required
-              />
+              <input type="email" class="form-control" v-model="email" required />
             </div>
             <div class="form-group">
               <label>Password</label>
-              <input
-                type="password"
-                class="form-control"
-                v-model="password"
-                required
-              />
+              <input type="password" class="form-control" v-model="password" required />
             </div>
-            <small class="form-text text-muted"
-              >By continuing, you agree to Simplecoding's Conditions of Use and
-              Privacy Notice.</small
-            >
             <button type="submit" class="btn btn-primary mt-2 py-0">
               Continue
-              <div
-                v-if="loading"
-                class="spinner-border spinner-border-sm"
-                role="status"
-              >
+              <div v-if="loading" class="spinner-border spinner-border-sm" role="status">
                 <span class="sr-only">Loading...</span>
               </div>
             </button>
           </form>
           <hr />
-          <small class="form-text text-muted pt-2 pl-4 text-center"
-            >New to Simplecoding?</small
-          >
           <p class="text-center">
-            <router-link
-              :to="{ name: 'Signup' }"
-              class="btn btn-dark text-center mx-auto px-5 py-1 mb-2"
-              >Create Your Simplecoding Account</router-link
-            >
+            <router-link :to="{ name: 'Signup' }" class="btn btn-dark text-center mx-auto px-5 py-1 mb-2">Create Your
+              Account</router-link>
           </p>
         </div>
       </div>
